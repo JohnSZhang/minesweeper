5285cc3cad87cdf12b4d18917791319ba6478196
john zhang <john.shimin.zhang@gmail.com>
2014-09-15 16:52:34 -0400

    add save functionality

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,9 +1,20 @@
 require_relative "board"
 require_relative "tile"
+require "yaml"
 class Minesweeper
   attr_reader :board
   def initialize
     @board = Board.new
+  end
+  def save
+    save = @board.to_yaml
+    file = File.open("save_file.txt", "w") << save
+    file.close
+  end
+
+  def load
+    file = File.read("save_file.txt")
+    @board = YAML::load(file)
   end
 
   def play
@@ -11,8 +22,16 @@ class Minesweeper
       self.board.render
       print "Pick a coordinate: "
       pick_coordinate = gets.chomp.split(' ')
-      pos = pick_coordinate.drop(1).map{|i| i.to_i}
-      self.board.process_tile(pos, pick_coordinate[0])
+      if pick_coordinate[0] == "s"
+        save
+        puts "Game was saved! Dont try and cheat the game"
+      elsif pick_coordinate[0] == "l"
+        load
+        puts "Game was loaded!"
+      else
+        pos = pick_coordinate.drop(1).map{|i| i.to_i}
+        self.board.process_tile(pos, pick_coordinate[0])
+      end
     end
     self.board.render
     puts "Game Over! *BOOM*" if self.board.lose?
